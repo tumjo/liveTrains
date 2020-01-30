506f55c7e23c973d3ecc0121cef5087fd43cda02
tumjo <tumikku@gmail.com>
2020-01-30 16:25:56 +0200

    Add routing to menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TrainsComponent } from './trains/trains.component';
+import { TrainsreturnComponent } from './trainsreturn/trainsreturn.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'trains',
+    component: TrainsComponent
+  },
+  {
+    path: 'trainsreturn',
+    component: TrainsreturnComponent
+  },
+  {
+    path: '',
+    redirectTo: '/trains',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: TrainsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
