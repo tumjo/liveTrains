4032a98bd84332e84aac051a004194d330f24755
tumjo <tumikku@gmail.com>
2020-01-30 14:46:58 +0200

    Add Angular Material navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { TrainsComponent } from './trains/trains.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialdesignModule } from './materialdesign/materialdesign.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialdesignModule
   ],
   providers: [],
   bootstrap: [AppComponent]
